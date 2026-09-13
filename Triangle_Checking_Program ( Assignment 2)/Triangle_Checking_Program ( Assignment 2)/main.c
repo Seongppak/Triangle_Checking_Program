8626381958e8221824a6b6d4ec5e0764c68ede72
Seongppak <wsh070309@gmail.com>
2026-09-13 18:20:25 +0900

    텍스트 출력 수정 ( 가장 큰 -> 가장 긴 )

--- Triangle_Checking_Program ( Assignment 2)/Triangle_Checking_Program ( Assignment 2)/main.c
+++ Triangle_Checking_Program ( Assignment 2)/Triangle_Checking_Program ( Assignment 2)/main.c
@@ -39,21 +39,21 @@ void check_triangle(x, y, z) // 삼각형 판별 함수 정의
 	if (x > y && x > z) // x가 가장 클 때
 	{
 		if (x < y + z)
-			printf("x의 길이가 가장 큰 삼각형입니다.");
+			printf("x의 길이가 가장 긴 삼각형입니다.");
 		else
 			printf("삼각형이 될 수 없습니다.");
 	}
 	if (y > x && y > z) // y가 가장 클 때
 	{
 		if (y < x + z)
-			printf("y의 길이가 가장 큰 삼각형입니다.");
+			printf("y의 길이가 가장 긴 삼각형입니다.");
 		else
 			printf("삼각형이 될 수 없습니다.");
 	}
 	if (z > y && z > x) // z가 가장 클 때
 	{
 		if (z < y + x)
-			printf("z의 길이가 가장 큰 삼각형입니다.");
+			printf("z의 길이가 가장 긴 삼각형입니다.");
 		else
 			printf("삼각형이 될 수 없습니다.");
 	}
